f96dc7399c3e3a92119321cff85ca0e7eae342c8
Lily Foote <code@lilyf.org>
2024-02-01 23:32:45 +0000

    Add virtualenvs module
    Move the remaining command implementations so `main.rs` now only handles
    parsing the command line and delegating to implementations.

--- src/main.rs
+++ src/main.rs
@@ -6,153 +6,16 @@ mod error;
 mod releases;
 mod shell;
 mod types;
-use crate::directories::{project_dir, project_file, python_dir, virtualenv_dir, virtualenvs_dir};
+mod virtualenvs;
 use crate::download::{download_python, print_available_downloads};
 use crate::error::Error;
-use crate::shell::{get_shell, print_shell_config, set_shell};
+use crate::shell::{print_shell_config, set_shell};
 use crate::types::Version;
-
-fn create_virtualenv(version: &Version, project: &str) -> Result<(), Error> {
-    let python = python_dir(version);
-    if !python.exists() {
-        download_python(version, false)?;
-    }
-    let next = std::fs::read_dir(&python)?
-        .next()
-        .unwrap_or_else(|| {
-            panic!(
-                "Expected subdirectory missing from downloaded python at {:?}.",
-                &python
-            )
-        })?
-        .path();
-    let python_executable = next.join("bin/python3");
-    let virtualenv = virtualenv_dir(project, version);
-    std::process::Command::new(python_executable)
-        .arg("-m")
-        .arg("venv")
-        .arg(virtualenv)
-        .output()?;
-    Ok(())
-}
-
-fn remove_virtualenv(project: &str, version: &Version) -> Result<(), Error> {
-    let virtualenv = virtualenv_dir(project, version);
-    std::fs::remove_dir_all(virtualenv)?;
-    Ok(())
-}
-
-fn remove_project(project: &str) -> Result<(), Error> {
-    std::fs::remove_dir_all(project_dir(project))?;
-    Ok(())
-}
-
-fn activate_virtualenv(version: &Version, project: &str) -> Result<(), Error> {
-    let virtualenv = virtualenv_dir(project, version);
-    if !virtualenv.exists() {
-        create_virtualenv(version, project)?
-    }
-    let path = std::env::var("PATH")?;
-    let path = format!("{}:{path}", virtualenv.join("bin").display());
-
-    let mut shell = std::process::Command::new(get_shell()?);
-    let shell = match project_directory(project)? {
-        Some(directory) => shell.current_dir(directory),
-        _ => &mut shell,
-    };
-    let python = python_dir(version).join("python");
-    let mut shell = shell
-        .env("VIRTUAL_ENV", &virtualenv)
-        .env("VIRTUAL_ENV_PROMPT", format!("{project} ({version}) "))
-        .env("PATH", path)
-        .env(
-            "TERMINFO_DIRS",
-            "/etc/terminfo:/lib/terminfo:/usr/share/terminfo",
-        )
-        .env("LD_LIBRARY_PATH", python.join("lib"))
-        .spawn()?;
-    shell.wait()?;
-    Ok(())
-}
-
-fn cd_site_packages(project: &str, version: &Version) -> Result<(), Error> {
-    let virtualenv = virtualenv_dir(project, version);
-    let lib = virtualenv.join("lib");
-    let next = std::fs::read_dir(&lib)?
-        .next()
-        .unwrap_or_else(|| {
-            panic!(
-                "Expected subdirectory missing from virtualenv at {:?}.",
-                &lib
-            )
-        })?
-        .path();
-    let site_packages = next.join("site-packages");
-
-    let mut shell = std::process::Command::new(get_shell()?)
-        .current_dir(site_packages)
-        .spawn()?;
-    shell.wait()?;
-    Ok(())
-}
-
-fn list_versions(path: std::path::PathBuf) -> Result<Vec<String>, Error> {
-    Ok(std::fs::read_dir(path)?
-        .collect::<Result<Vec<_>, _>>()?
-        .into_iter()
-        .map(|version| {
-            version
-                .file_name()
-                .to_str()
-                .expect("Could not convert a version to utf-8.")
-                .to_string()
-        })
-        .collect::<Vec<_>>())
-}
-
-fn print_project_versions(project: String) -> Result<(), Error> {
-    let virtualenvs = project_dir(&project);
-    let versions = list_versions(virtualenvs)?;
-    println!("{}", versions.join(" "));
-    Ok(())
-}
-
-fn print_all_versions() -> Result<(), Error> {
-    let projects = virtualenvs_dir();
-    for project in std::fs::read_dir(projects)? {
-        let project = project?;
-        let versions = list_versions(project.path())?;
-        println!(
-            "{}: {}",
-            project
-                .file_name()
-                .to_str()
-                .expect("Could not convert a project directory name to utf-8"),
-            versions.join(" ")
-        );
-    }
-    Ok(())
-}
-
-fn set_project_directory(project: &str, default_directory: &str) -> Result<(), Error> {
-    std::fs::write(project_file(project), default_directory)?;
-    Ok(())
-}
-
-fn unset_project_directory(project: &str) -> Result<(), Error> {
-    std::fs::remove_file(project_file(project))?;
-    Ok(())
-}
-
-fn project_directory(project: &str) -> Result<Option<String>, Error> {
-    match std::fs::read_to_string(project_file(project)) {
-        Ok(default_directory) => Ok(Some(default_directory)),
-        Err(err) => match err.kind() {
-            std::io::ErrorKind::NotFound => Ok(None),
-            _ => Err(err)?,
-        },
-    }
-}
+use crate::virtualenvs::{
+    activate_virtualenv, cd_site_packages, create_virtualenv, print_all_versions,
+    print_project_versions, remove_project, remove_virtualenv, set_project_directory,
+    unset_project_directory,
+};
 
 #[derive(Parser)]
 #[command(author, version, about, long_about=None)]
---
+++ src/virtualenvs.rs
@@ -0,0 +1,147 @@
+use crate::directories::{project_dir, project_file, python_dir, virtualenv_dir, virtualenvs_dir};
+use crate::download::download_python;
+use crate::error::Error;
+use crate::shell::get_shell;
+use crate::types::Version;
+
+pub fn create_virtualenv(version: &Version, project: &str) -> Result<(), Error> {
+    let python = python_dir(version);
+    if !python.exists() {
+        download_python(version, false)?;
+    }
+    let next = std::fs::read_dir(&python)?
+        .next()
+        .unwrap_or_else(|| {
+            panic!(
+                "Expected subdirectory missing from downloaded python at {:?}.",
+                &python
+            )
+        })?
+        .path();
+    let python_executable = next.join("bin/python3");
+    let virtualenv = virtualenv_dir(project, version);
+    std::process::Command::new(python_executable)
+        .arg("-m")
+        .arg("venv")
+        .arg(virtualenv)
+        .output()?;
+    Ok(())
+}
+
+pub fn remove_virtualenv(project: &str, version: &Version) -> Result<(), Error> {
+    let virtualenv = virtualenv_dir(project, version);
+    std::fs::remove_dir_all(virtualenv)?;
+    Ok(())
+}
+
+pub fn remove_project(project: &str) -> Result<(), Error> {
+    std::fs::remove_dir_all(project_dir(project))?;
+    Ok(())
+}
+
+pub fn set_project_directory(project: &str, default_directory: &str) -> Result<(), Error> {
+    std::fs::write(project_file(project), default_directory)?;
+    Ok(())
+}
+
+pub fn unset_project_directory(project: &str) -> Result<(), Error> {
+    std::fs::remove_file(project_file(project))?;
+    Ok(())
+}
+
+fn project_directory(project: &str) -> Result<Option<String>, Error> {
+    match std::fs::read_to_string(project_file(project)) {
+        Ok(default_directory) => Ok(Some(default_directory)),
+        Err(err) => match err.kind() {
+            std::io::ErrorKind::NotFound => Ok(None),
+            _ => Err(err)?,
+        },
+    }
+}
+
+pub fn activate_virtualenv(version: &Version, project: &str) -> Result<(), Error> {
+    let virtualenv = virtualenv_dir(project, version);
+    if !virtualenv.exists() {
+        create_virtualenv(version, project)?
+    }
+    let path = std::env::var("PATH")?;
+    let path = format!("{}:{path}", virtualenv.join("bin").display());
+
+    let mut shell = std::process::Command::new(get_shell()?);
+    let shell = match project_directory(project)? {
+        Some(directory) => shell.current_dir(directory),
+        _ => &mut shell,
+    };
+    let python = python_dir(version).join("python");
+    let mut shell = shell
+        .env("VIRTUAL_ENV", &virtualenv)
+        .env("VIRTUAL_ENV_PROMPT", format!("{project} ({version}) "))
+        .env("PATH", path)
+        .env(
+            "TERMINFO_DIRS",
+            "/etc/terminfo:/lib/terminfo:/usr/share/terminfo",
+        )
+        .env("LD_LIBRARY_PATH", python.join("lib"))
+        .spawn()?;
+    shell.wait()?;
+    Ok(())
+}
+
+pub fn cd_site_packages(project: &str, version: &Version) -> Result<(), Error> {
+    let virtualenv = virtualenv_dir(project, version);
+    let lib = virtualenv.join("lib");
+    let next = std::fs::read_dir(&lib)?
+        .next()
+        .unwrap_or_else(|| {
+            panic!(
+                "Expected subdirectory missing from virtualenv at {:?}.",
+                &lib
+            )
+        })?
+        .path();
+    let site_packages = next.join("site-packages");
+
+    let mut shell = std::process::Command::new(get_shell()?)
+        .current_dir(site_packages)
+        .spawn()?;
+    shell.wait()?;
+    Ok(())
+}
+
+fn list_versions(path: std::path::PathBuf) -> Result<Vec<String>, Error> {
+    Ok(std::fs::read_dir(path)?
+        .collect::<Result<Vec<_>, _>>()?
+        .into_iter()
+        .map(|version| {
+            version
+                .file_name()
+                .to_str()
+                .expect("Could not convert a version to utf-8.")
+                .to_string()
+        })
+        .collect::<Vec<_>>())
+}
+
+pub fn print_project_versions(project: String) -> Result<(), Error> {
+    let virtualenvs = project_dir(&project);
+    let versions = list_versions(virtualenvs)?;
+    println!("{}", versions.join(" "));
+    Ok(())
+}
+
+pub fn print_all_versions() -> Result<(), Error> {
+    let projects = virtualenvs_dir();
+    for project in std::fs::read_dir(projects)? {
+        let project = project?;
+        let versions = list_versions(project.path())?;
+        println!(
+            "{}: {}",
+            project
+                .file_name()
+                .to_str()
+                .expect("Could not convert a project directory name to utf-8"),
+            versions.join(" ")
+        );
+    }
+    Ok(())
+}
