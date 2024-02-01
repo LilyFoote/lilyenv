36c1a1446189edeed193d6960e49cb5284c0cda4
Lily Foote <code@lilyf.org>
2024-02-01 23:17:17 +0000

    Add download module

--- src/main.rs
+++ src/main.rs
@@ -1,112 +1,16 @@
-use bzip2::read::BzDecoder;
 use clap::{Parser, Subcommand};
-use flate2::read::GzDecoder;
-use std::fs::File;
-use std::path::Path;
-use tar::Archive;
-use url::Url;
 
 mod directories;
+mod download;
 mod error;
 mod releases;
 mod types;
 use crate::directories::{
-    downloads_dir, project_dir, project_file, python_dir, shell_file, virtualenv_dir,
-    virtualenvs_dir,
+    project_dir, project_file, python_dir, shell_file, virtualenv_dir, virtualenvs_dir,
 };
+use crate::download::{download_python, print_available_downloads};
 use crate::error::Error;
-use crate::releases::{cpython_releases, pypy_releases};
-use crate::types::{Interpreter, Version};
-
-fn download_python(version: &Version, upgrade: bool) -> Result<(), Error> {
-    match version.interpreter {
-        Interpreter::CPython => download_cpython(version, upgrade),
-        Interpreter::PyPy => download_pypy(version, upgrade),
-    }
-}
-
-fn download_cpython(version: &Version, upgrade: bool) -> Result<(), Error> {
-    let python_dir = python_dir(version);
-    if !upgrade && python_dir.exists() {
-        return Ok(());
-    }
-
-    let downloads = downloads_dir();
-    std::fs::create_dir_all(&downloads)?;
-
-    let rt = tokio::runtime::Builder::new_current_thread()
-        .enable_all()
-        .build()?;
-    let python = match rt
-        .block_on(cpython_releases())?
-        .into_iter()
-        .find(|python| python.version.compatible(version))
-    {
-        Some(python) => python,
-        None => {
-            return Err(Error::VersionNotFound(version.to_string()));
-        }
-    };
-    let path = downloads.join(python.name);
-    if upgrade || !path.exists() {
-        download_file(python.url, &path)?;
-    }
-    extract_tar_gz(&path, &python_dir)?;
-    Ok(())
-}
-
-fn download_pypy(version: &Version, upgrade: bool) -> Result<(), Error> {
-    let python_dir = python_dir(version);
-    if !upgrade && python_dir.exists() {
-        return Ok(());
-    }
-
-    let downloads = downloads_dir();
-    std::fs::create_dir_all(&downloads)?;
-
-    let python = match pypy_releases()?
-        .into_iter()
-        .find(|python| python.version.compatible(version))
-    {
-        Some(python) => python,
-        None => {
-            return Err(Error::VersionNotFound(version.to_string()));
-        }
-    };
-    let path = downloads.join(python.name);
-    if upgrade || !path.exists() {
-        download_file(python.url, &path)?;
-    }
-    extract_tar_bz2(&path, &python_dir)?;
-    Ok(())
-}
-
-fn download_file(url: Url, target: &Path) -> Result<(), Error> {
-    let client = reqwest::blocking::Client::builder()
-        .user_agent("lilyenv")
-        .build()?;
-    let response = client.get(url).send()?;
-    let mut file = File::create(target)?;
-    let mut content = std::io::Cursor::new(response.bytes()?);
-    std::io::copy(&mut content, &mut file)?;
-    Ok(())
-}
-
-fn extract_tar_gz(source: &Path, target: &Path) -> Result<(), std::io::Error> {
-    let tar_gz = File::open(source)?;
-    let tar = GzDecoder::new(tar_gz);
-    let mut archive = Archive::new(tar);
-    archive.unpack(target)?;
-    Ok(())
-}
-
-fn extract_tar_bz2(source: &Path, target: &Path) -> Result<(), std::io::Error> {
-    let tar_gz = File::open(source)?;
-    let tar = BzDecoder::new(tar_gz);
-    let mut archive = Archive::new(tar);
-    archive.unpack(target)?;
-    Ok(())
-}
+use crate::types::Version;
 
 fn create_virtualenv(version: &Version, project: &str) -> Result<(), Error> {
     let python = python_dir(version);
@@ -189,23 +93,6 @@ fn cd_site_packages(project: &str, version: &Version) -> Result<(), Error> {
         .current_dir(site_packages)
         .spawn()?;
     shell.wait()?;
-    Ok(())
-}
-
-fn print_available_downloads() -> Result<(), Error> {
-    let rt = tokio::runtime::Builder::new_current_thread()
-        .enable_all()
-        .build()?;
-    let mut releases = rt.block_on(cpython_releases())?;
-    releases.sort_unstable_by_key(|p| p.version);
-    for python in releases {
-        println!("{} ({})", python.version, python.release_tag);
-    }
-    let mut pypy_releases = pypy_releases()?;
-    pypy_releases.sort_unstable_by_key(|p| p.version);
-    for python in pypy_releases {
-        println!("{} ({})", python.version, python.release_tag);
-    }
     Ok(())
 }
 
